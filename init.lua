bd8e3e8f71ea6639b300854acbd42d578aa791a1
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2024-11-22 22:30:46 +0000

    Remove moving text up/down and map 0 to first char in line

--- init.lua
+++ init.lua
@@ -157,23 +157,8 @@ vim.api.nvim_set_keymap('n', '<leader>ss', ':setlocal spell!<CR>', {
     silent = true
 })
 
--- Moving text up and down
-vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', {
-    noremap = true,
-    silent = true
-})
-vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", {
-    noremap = true,
-    silent = true
-})
+-- Map 0 to go to the first character of the line
+vim.api.nvim_set_keymap('n', '0', '^', { noremap = true, silent = true })
 
 -- Window Navigation --------------------------------------------------------
 
