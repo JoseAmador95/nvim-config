96dbe9eabd3429a24ed04a8a497c3671ebffb2a5
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2026-02-05 00:23:07 +0000

    Added keybind hints

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  cond = function() return not vim.g.vscode end,
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {
+    preset = "helix",
+    delay = 1000,
+  }
+}
