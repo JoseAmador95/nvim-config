53c8379b7ae1a69d4b2da86c72c9d7370932924e
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2026-02-09 19:01:10 +0000

    Telescope open file in new tab by default

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,41 +15,51 @@ return {
 			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
 			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
 		},
-		opts = {
-			defaults = {
-				cwd = vim.fn.getcwd(),
-                file_ignore_patterns = {
-                    "node_modules",
-                    ".git"
-                },
-				mappings = {
-					i = {
-						["<C-j>"] = "move_selection_next",
-						["<C-k>"] = "move_selection_previous",
-						["<C-h>"] = "move_selection_previous",
-						["<C-l>"] = "move_selection_next",
-						["<C-q>"] = "close",
+
+		opts = function()
+			local actions = require("telescope.actions")
+
+			return {
+				defaults = {
+					cwd = vim.fn.getcwd(),
+					file_ignore_patterns = { "node_modules", ".git" },
+
+					mappings = {
+						i = {
+							["<C-j>"] = "move_selection_next",
+							["<C-k>"] = "move_selection_previous",
+							["<C-h>"] = "move_selection_previous",
+							["<C-l>"] = "move_selection_next",
+							["<C-q>"] = "close",
+
+							-- ⭐ Open in NEW TAB by default (Insert mode)
+							["<CR>"] = actions.select_tab,
+						},
+						n = {
+							["j"] = "move_selection_next",
+							["k"] = "move_selection_previous",
+							["h"] = "move_selection_previous",
+							["l"] = "move_selection_next",
+							["q"] = "close",
+
+							-- ⭐ Open in NEW TAB by default (Normal mode)
+							["<CR>"] = actions.select_tab,
+						},
 					},
-					n = {
-						["j"] = "move_selection_next",
-						["k"] = "move_selection_previous",
-						["h"] = "move_selection_previous",
-						["l"] = "move_selection_next",
-						["q"] = "close",
+				},
+
+				pickers = {
+					find_files = {
+						hidden = true,
+						no_ignore = false,
+					},
+					live_grep = {
+						additional_args = function()
+							return { "--hidden" }
+						end,
 					},
 				},
-			},
-			pickers = {
-				find_files = {
-					hidden = true,
-					no_ignore = false,
-				},
-				live_grep = {
-					additional_args = function()
-						return { "--hidden" }
-					end,
-				},
-			},
-		},
+			}
+		end,
 	},
 }
