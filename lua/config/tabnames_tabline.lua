2b0ff247d3ec569ef6acc34dc986d837b0087483
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2026-02-09 19:02:04 +0000

    switch tab on click

--- lua/config/tabnames_tabline.lua
+++ lua/config/tabnames_tabline.lua
@@ -3,17 +3,18 @@ local tabnames = require("config.tabnames")
 local M = {}
 
 function M.tabline()
-  local s = ""
-  for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
-    local nr = vim.api.nvim_tabpage_get_number(tab)
-    local name = tabnames.tab_title(tab)
+	local s = ""
+	for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
+		local nr = vim.api.nvim_tabpage_get_number(tab)
+		local name = tabnames.tab_title(tab)
 
-    local hl = (tab == vim.api.nvim_get_current_tabpage()) and "%#TabLineSel#" or "%#TabLine#"
+		local hl = (tab == vim.api.nvim_get_current_tabpage()) and "%#TabLineSel#" or "%#TabLine#"
 
-    s = s .. hl .. " " .. nr .. ":" .. name .. " "
-  end
-  s = s .. "%#TabLineFill#"
-  return s
+		-- Add %@...@ region to make the tab clickable
+		s = s .. "%" .. nr .. "T" .. hl .. " " .. nr .. ":" .. name .. " "
+	end
+	s = s .. "%T" .. "%#TabLineFill#"
+	return s
 end
 
 return M
