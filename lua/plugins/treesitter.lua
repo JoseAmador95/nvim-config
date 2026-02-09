8ed209e76d1c088d8d10d7ad77e65fa0bd73eb38
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2026-02-09 19:00:42 +0000

    Upgrade treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,29 +1,58 @@
+-- lua/plugins/treesitter.lua
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		cond = function()
-			return not vim.g.vscode
-		end,
-		build = ":TSUpdate",
-		event = { "BufReadPost", "BufNewFile" },
-		opts = {
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "gnn",
-					node_incremental = "grn",
-					scope_incremental = "grc",
-					node_decremental = "grm",
-				},
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.config").setup(opts)
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false,
+    lazy = false,       -- Treesitter itself should not be lazy-loaded
+    build = ":TSUpdate",
+    config = function()
+      -- Modern nvim-treesitter API (rewrite):
+      -- Prefer this; if you still need the legacy branch, we can switch later.
+      require("nvim-treesitter").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+    -- If you want to skip inside VSCode:
+    cond = function() return not vim.g.vscode end,
+  },
+
+  -- Always-on context (loads at startup)
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    lazy = false,
+    opts = {},
+    cond = function() return not vim.g.vscode end,
+  },
+
+  -- Always-on rainbow delimiters (loads at startup)
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    lazy = false,
+    config = function()
+      local rd = require("rainbow-delimiters")
+
+      require("rainbow-delimiters.setup").setup({
+        strategy = {
+          [""] = rd.strategy.global,
+          -- `local` is a Lua keyword; use bracket syntax:
+          commonlisp = rd.strategy["local"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          lua = "rainbow-blocks",
+        },
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+        },
+      })
+    end,
+    cond = function() return not vim.g.vscode end,
+  },
 }
