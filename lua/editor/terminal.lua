77f52bfa3c032e94a325a5d9b3ae1824d966f672
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2026-02-15 15:33:32 +0000

    Add mappings for log highlight

--- lua/editor/terminal.lua
+++ lua/editor/terminal.lua
@@ -1,3 +1,36 @@
+local function toggle_log_highlight()
+	local bufnr = vim.api.nvim_get_current_buf()
+	local ft = vim.bo[bufnr].filetype
+
+	if ft == "log" then
+		local prev = vim.b[bufnr].log_highlight_prev_ft or "text"
+		vim.bo[bufnr].filetype = prev
+		vim.b[bufnr].log_highlight_enabled = false
+		return
+	end
+
+	vim.b[bufnr].log_highlight_prev_ft = ft
+	vim.bo[bufnr].filetype = "log"
+	vim.b[bufnr].log_highlight_enabled = true
+end
+
+local function toggle_log_wrap()
+	vim.wo.wrap = not vim.wo.wrap
+	vim.wo.linebreak = vim.wo.wrap
+end
+
+vim.api.nvim_create_user_command("CloseAll", function()
+	vim.cmd("confirm qall")
+end, { desc = "Close all with confirmation" })
+
+vim.api.nvim_create_user_command("ToggleLogHighlight", toggle_log_highlight, {
+	desc = "Toggle log highlighting",
+})
+
+vim.api.nvim_create_user_command("ToggleLogWrap", toggle_log_wrap, {
+	desc = "Toggle log wrap",
+})
+
 vim.keymap.set("i", "jj", "<Esc>", {
 	noremap = true,
 	silent = true,
@@ -16,10 +49,22 @@ vim.keymap.set("n", "<leader>q", ":q<CR>", {
 	desc = "Quit",
 })
 
-vim.keymap.set("n", "<leader>Q", ":q!<CR>", {
+vim.keymap.set("n", "<leader>Q", "<cmd>CloseAll<cr>", {
 	noremap = true,
 	silent = true,
-	desc = "Force quit",
+	desc = "Close all (confirm)",
+})
+
+vim.keymap.set("n", "<leader>lh", "<cmd>ToggleLogHighlight<cr>", {
+	noremap = true,
+	silent = true,
+	desc = "Toggle log highlight",
+})
+
+vim.keymap.set("n", "<leader>lw", "<cmd>ToggleLogWrap<cr>", {
+	noremap = true,
+	silent = true,
+	desc = "Toggle log wrap",
 })
 
 vim.keymap.set("n", "<leader>x", ":x<CR>", {
