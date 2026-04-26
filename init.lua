45e9e6effa9c5400e607f62e9cd97c4628e56926
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2026-04-26 22:38:57 +0100

    Change leader to " "

--- init.lua
+++ init.lua
@@ -11,8 +11,8 @@ vim.opt.termguicolors = true
 vim.cmd("syntax enable")
 
 -- Leader Key
-vim.g.mapleader = "," -- Change to any preferred leader key
-vim.g.maplocalleader = "," -- Set a local leader key
+vim.g.mapleader = " " -- Change to any preferred leader key
+vim.g.maplocalleader = " " -- Set a local leader key
 
 -- Enable persistent undo and set undo file directory
 vim.opt.undofile = true
