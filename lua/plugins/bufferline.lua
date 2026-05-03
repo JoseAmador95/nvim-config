41cef4a40ec16af550b62b16496b4328355d3742
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2026-05-03 22:37:47 +0100

    Use tabs in bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -9,7 +9,7 @@ return {
 	config = function()
 		require("bufferline").setup({
 			options = {
-				mode = "buffers",
+				mode = "tabs",
 				diagnostics = "nvim_lsp",
 				show_buffer_icons = true,
 				show_buffer_close_icons = true,
