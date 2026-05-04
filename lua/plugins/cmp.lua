0f7974d29fdbe4a0a46e8779f9b2ac3c54ac743b
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2026-05-04 09:21:15 +0100

    Add cmp to cmdline and search

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,12 +3,13 @@ return {
 	cond = function()
 		return not vim.g.vscode
 	end,
-	event = "InsertEnter",
+	event = { "InsertEnter", "CmdlineEnter" },
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-cmdline",
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
 		"rafamadriz/friendly-snippets",
@@ -56,6 +57,24 @@ return {
 				{ name = "buffer" },
 				{ name = "path" },
 			}),
+		})
+
+		-- Cmdline completion for ":"
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{ name = "cmdline" },
+			}),
+		})
+
+		-- Cmdline completion for "/" and "?"
+		cmp.setup.cmdline({ "/", "?" }, {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
 		})
 	end,
 }
